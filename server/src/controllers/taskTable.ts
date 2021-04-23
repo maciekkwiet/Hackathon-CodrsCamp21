c023f65e7c0657dc0918610f69a20db7c62f2b2b
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-23 22:23:34 +0200

    Refactor api

---
+++ server/src/controllers/taskTable.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response } from 'express';
+import { rooms } from '@models/Rooms';
+
+const router = Router();
+
+interface Query {
+  roomId?: string;
+}
+
+router.get('/', async (req: Request<any, any, any, Query>, res: Response) => {
+  try {
+    const { roomId } = req.query;
+    if (typeof roomId !== 'string') return res.status(400).json({ error: 'Invalid parameter' });
+    const room = rooms.getRoom(roomId);
+    res.json({ data: room.tableData });
+  } catch (ex) {
+    console.error(ex);
+  }
+});
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { roomId, data } = req.query;
+    if (typeof roomId !== 'string') return res.status(400).json({ error: 'Invalid parameter' });
+    const room = rooms.getRoom(roomId);
+    room.setTableData(data)
+    res.json({ data: room.tableData });
+  } catch (ex) {
+    console.error(ex);
+  }
+});
+
+export { router as taskTableController };
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { sessionController } from '@controllers/session';
+import { taskTableController } from '@controllers/taskTable';
 
 const router = Router();
 
 router.use('/session', sessionController);
+router.use('/taskTable', taskTableController);
 
 export { router as apiController };
