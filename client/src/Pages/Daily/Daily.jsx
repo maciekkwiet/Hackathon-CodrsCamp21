814f4ded342284ea68ba8144383323a30056781b
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-04-24 02:02:47 +0200

    belkot

--- client/src/Pages/Daily/Daily.jsx
+++ client/src/Pages/Daily/Daily.jsx
@@ -1,18 +1,49 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FaAngleLeft } from 'react-icons/fa';
 import { Bordzik } from '../../Components/Bordzik/Bordzik';
-import { Container, Navi, CallMe, Icons, StyledLink, StyledButton } from '../Daily/DailyStyles';
+import { Container, Navi, CallMe, Icons, StyledButton, SideContainer, Title, Person, Name } from '../Daily/DailyStyles';
 
 export const DailyPage = () => {
+  const [show, setShow] = useState(false);
+  let history = useHistory();
+
+  function handleReturn() {
+    history.push('/');
+  }
+
   return (
     <Container>
       <Navi>
-        <CallMe></CallMe>
+        <CallMe show={show}></CallMe>
         <Icons>
-          <StyledLink to={'/'}>
-            <StyledButton>Back</StyledButton>
-          </StyledLink>
-          <StyledButton>List</StyledButton>
+          <StyledButton onClick={handleReturn}>
+            <i>{FaAngleLeft}</i>Back
+          </StyledButton>
+          <StyledButton onClick={() => setShow(!show)}>Lista osob na daily</StyledButton>
         </Icons>
       </Navi>
+      <SideContainer show={show}>
+        <Title>Online mates:</Title>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+        <Person>
+          <Name>Robert Maklowicz</Name>
+        </Person>
+      </SideContainer>
       <Bordzik></Bordzik>
     </Container>
   );
--- client/src/Pages/Daily/DailyStyles.jsx
+++ client/src/Pages/Daily/DailyStyles.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
@@ -7,38 +6,66 @@ export const Container = styled.div`
   justify-content: stretch;
 `;
 
-//Styles for mini Navbar
+//,,Mini navbar" on top
 export const Navi = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   min-height: 50px;
-  background: pink;
+  background: red;
 `;
 
+//Link to meeting
 export const CallMe = styled.div`
-  display: none;
-  width: 90%;
-  background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 65%;
+  transform: ${({ show }) => (show ? 'translateX(0)' : 'translateX(-100%)')};
+  background: yellow;
+`;
+
+//Online people
+export const SideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  top: 50px;
+  right: 0;
+  height: 100%;
+  width: 35%;
+  transform: ${({ show }) => (show ? 'translateX(0)' : 'translateX(100%)')};
+  z-index: 5;
+  background: yellow;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  padding: 25px 0;
+  font-size: 1.5rem;
+`;
+
+export const Person = styled.div`
+  text-align: center;
+  width: 70%;
+  padding: 7px;
+  font-size: 1.25rem;
+  border-bottom: 1px solid #1a1b25;
+`;
+
+export const Name = styled.p`
+  color: black;
 `;
 
 //Styles for round icons
 export const Icons = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: stretch;
   align-content: center;
   align-items: center;
-  width: 10%;
+  width: 35%;
 `;
 
-export const StyledLink = styled(Link)`
-  display: flex;
-  padding: 0;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-`;
 export const StyledButton = styled.button`
   padding: 0;
   display: flex;
@@ -47,6 +74,9 @@ export const StyledButton = styled.button`
   width: 100%;
   height: 100%;
   border: none;
-  border-radius: 50%;
-  background: yellow;
+  background: red;
+
+  &:hover {
+    background: yellow;
+  }
 `;
