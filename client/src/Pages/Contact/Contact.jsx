e512fbc96d783f684ec0a069a108808e65b06435
unknown <maciek.kwiet36@gmail.com>
2021-04-24 17:01:47 +0200

    add conact page

--- client/src/Pages/Contact/Contact.jsx
+++ client/src/Pages/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSocket } from 'socketio-hooks';
+import Carousel from 'react-elastic-carousel'
+import './Contact.css';
 import { FaAngleLeft, FaUserFriends } from 'react-icons/fa';
 import {
   Container,
@@ -9,6 +11,8 @@ import {
   StyledButton,
   StyledButton1
 } from '../Daily/DailyStyles';
+
+
 
 export const ContactPage = () => {
   const [show, setShow] = useState(false);
@@ -38,10 +42,17 @@ export const ContactPage = () => {
           <StyledButton1 onClick={() => setShow(!show)}>
             <FaUserFriends size={'2rem'} />
           </StyledButton1>
-          <div>Contact</div>
         </Icons>
       </Navi>
   <div>
+  <Carousel itemsToShow={1} className="styling-example" style={{marginTop:50, position:'absolute'}}>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+</Carousel>
   </div>
     </Container>
   );
