7e67438f02f6857c33039b1e66a402f3f51d65ce
Mariusz Olszewski <ssssspam11@gmail.com>
2021-04-24 10:15:56 +0200

    Add navbar to beer

--- client/src/Pages/Beer/Beer.jsx
+++ client/src/Pages/Beer/Beer.jsx
@@ -1,5 +1,65 @@
 import Callendar from '../../Components/Callendar/CustomTimeline';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FaAngleLeft, FaUserFriends } from 'react-icons/fa';
+import { Bordzik } from '../../Components/Bordzik/Bordzik';
+import { Container, Navi, Icons, StyledButton, SideContainer, Title, Person, Name } from '../Daily/DailyStyles';
+
+export const peoples = [
+  {
+    name: 'Anna',
+    surname: 'Wanna',
+  },
+  {
+    name: 'Andrzej',
+    surname: 'Wanna',
+  },
+  {
+    name: 'Marek',
+    surname: 'Kubek',
+  },
+  {
+    name: 'Janina',
+    surname: 'Koc',
+  },
+  {
+    name: 'Marcin',
+    surname: 'Drzwi',
+  },
+];
 
 export const BeerPage = () => {
-  return <Callendar />;
+  const [show, setShow] = useState(false);
+  let history = useHistory();
+
+  function handleReturn() {
+    history.push('/menu');
+  } 
+    return (
+    <Container>
+      <Navi>
+        <Icons>
+          <StyledButton onClick={handleReturn}>
+            <FaAngleLeft size={'2rem'} />
+          </StyledButton>
+          <StyledButton onClick={() => setShow(!show)}>
+            <FaUserFriends size={'2rem'} />
+          </StyledButton>
+        </Icons>
+      </Navi>
+      <SideContainer show={show}>
+        <Title>Na spotkaniu:</Title>
+        {peoples.map(person => {
+          return (
+            <Person>
+              <Name>{`${person.name} ${person.surname}`}</Name>
+            </Person>
+          );
+        })}
+      </SideContainer>
+      <Bordzik></Bordzik>
+    <Callendar />  
+    </Container>
+    
+  );
 };
