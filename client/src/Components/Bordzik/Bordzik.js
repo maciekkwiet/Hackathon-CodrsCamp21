31e379b781f8df03443206c80e368e1840ad3dca
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-24 12:18:25 +0200

    asdrt

--- client/src/Components/Bordzik/Bordzik.js
+++ client/src/Components/Bordzik/Bordzik.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useEmit, useSocket } from 'socketio-hooks';
 
 import { StyledBoard } from '../Bordzik/BordzikStyles';
 
 export const Bordzik = () => {
+  const changeTable = useEmit('CHANGE_TABLE');
+  let sessionId = localStorage.getItem('sessionId');
   const [data, setData] = useState({
     lanes: [
       {
@@ -17,12 +20,22 @@ export const Bordzik = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      let sessionId = localStorage.getItem('sessionId');
       const { data } = await axios.get('http://localhost:4000/api/taskTable/', { params: { roomId: sessionId } });
       setData(data.data);
     };
     fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   const setDataAPI = async () => {
+  //     changeTable({ roomId: sessionId }, ({ room, token }) => {});
+  //   };
+  //   setDataAPI();
+  // }, [data]);
+
+  useSocket('CHANGED_TABLE', newData => {
+    setData(newData);
+  });
 
   return <StyledBoard data={data} editable editLaneTitle draggable canAddLanes onDataChange={setData}></StyledBoard>;
 };
