d1fda4c9caf6082c953b34ec701332f3712ee7dc
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-24 05:35:50 +0200

    secket try

--- server/src/routes/socket.ts
+++ server/src/routes/socket.ts
@@ -1,11 +1,11 @@
-import * as socketio from "socket.io";
+const socket = require("socket.io");
 import * as http from "http";
 import { onUserJoin } from "@controllers/onUserJoin";
 
 const socketController = (server: http.Server): void => {
-  const io = socketio(server);
+  const io = socket(server);
 
-  io.on("connect", (socket: socketio.Socket): void => {
+  io.on("connect", (socket: any): void => {
     console.log("DUPAV33");
     socket.on("USER_JOINED", onUserJoin(io, socket));
     console.log("DUPAV11");
