e7ebb5c4d15ccbe1c4318dc975a2548ad3a0a2cc
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-24 05:26:18 +0200

    testtesttest

--- server/src/app.ts
+++ server/src/app.ts
@@ -43,6 +43,8 @@ server.listen(port, () => console.log(`Server listening on port ${port}`));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
+socketController(server);
+
 app.use(router);
 
 app.get("/", (request, response) => {
