f94bc8c5a255c81388628b9db36c1959389f20f6
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-24 11:27:30 +0200

    FIx table

--- client/src/Components/Bordzik/Bordzik.js
+++ client/src/Components/Bordzik/Bordzik.js
@@ -7,30 +7,9 @@ export const Bordzik = () => {
   const [data, setData] = useState({
     lanes: [
       {
-        id: 'lane1',
-        title: 'Do zrobienia',
-        label: '2/5',
-        cards: [
-          { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false },
-          {
-            id: 'Card2',
-            title: 'Pay Rent',
-            description: 'Transfer via NEFT',
-            label: '5 mins',
-            metadata: { sha: 'be312a1' },
-          },
-        ],
-      },
-      {
-        id: 'lane 2',
-        title: 'W trakcie',
-        label: '0/5',
-        cards: [],
-      },
-      {
-        id: 'lane3',
-        title: 'Zrobione',
-        label: '0/5',
+        id: '',
+        title: '',
+        label: '',
         cards: [],
       },
     ],
@@ -39,13 +18,11 @@ export const Bordzik = () => {
   useEffect(() => {
     const fetchData = async () => {
       let sessionId = localStorage.getItem('sessionId');
-      const result = await axios.get('http://localhost:4000/api/taskTable/', { params: { roomId: sessionId } });
-      console.log(result);
-      setData(result.data);
+      const { data } = await axios.get('http://localhost:4000/api/taskTable/', { params: { roomId: sessionId } });
+      setData(data.data);
     };
     fetchData();
   }, []);
 
-  console.log(data);
   return <StyledBoard data={data} editable editLaneTitle draggable canAddLanes onDataChange={setData}></StyledBoard>;
 };
