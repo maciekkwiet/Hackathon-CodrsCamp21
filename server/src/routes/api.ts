15f2ef17202d7ed3472d717a9691fdd1d59cf83e
maciekkwiet <maciej.kwietniewski@pronos.eu>
2021-04-24 17:14:18 +0200

    getUsers

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
-import { sessionController } from '@controllers/session';
-import { taskTableController } from '@controllers/taskTable';
+import { Router } from "express";
+import { sessionController } from "@controllers/session";
+import { taskTableController } from "@controllers/taskTable";
+import { usersController } from "@controllers/getUsers";
 
 const router = Router();
 
-router.use('/session', sessionController);
-router.use('/taskTable', taskTableController);
+router.use("/session", sessionController);
+router.use("/taskTable", taskTableController);
+router.use("/users", usersController);
 
 export { router as apiController };
